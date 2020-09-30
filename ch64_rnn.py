b60e8d17ed5807fa77b0848ff1f6618e71071317
Yuzhi Sun <17745615596@163.com>
2020-09-30 15:58:13 +0800

    X_20200930

--- ch64_rnn.py
+++ ch64_rnn.py
@@ -74,3 +74,71 @@ def predict_rnn(prefix, num_chars, rnn, params, init_rnn_state,
 predict_rnn('分开', 10, rnn, params, init_rnn_state, num_hiddens, vocab_size,
             ctx, idx_to_char, char_to_idx)
 
+# 梯度裁剪 防止梯度爆炸
+def grad_clipping(params, theta, ctx):
+    norm = nd.array([0], ctx)
+    for param in params:
+        norm += (param.grad ** 2).sum()
+    norm = norm.sqrt().asscalar()
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta/norm
+
+def train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
+                          vocab_size, ctx, corpus_indices, idx_to_char,
+                          char_to_idx, is_random_iter, num_epochs, num_steps,
+                          lr, clipping_theta, batch_size, pred_period,
+                          pred_len, prefixes):
+    if is_random_iter:
+        data_iter_fn = d2l.data_iter_random
+    else:
+        data_iter_fn = d2l.data_iter_consecutive
+    params = get_params()
+    loss = gloss.SoftmaxCrossEntropyLoss()
+
+    for epoch in range(num_epochs):
+        if not is_random_iter:
+            state = init_rnn_state(batch_size, num_hiddens, ctx)
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = data_iter_fn(corpus_indices, batch_size, num_steps, ctx)
+        for X, Y in data_iter:
+            if is_random_iter:
+                state = init_rnn_state(batch_size, num_hiddens, ctx)
+            else:
+                for s in state:
+                    s.detach()
+            with autograd.record():
+                inputs = to_onehot(X, vocab_size)
+                (outputs, state) = rnn(inputs, state, params)
+                outputs = nd.concat(*outputs, dim=0)
+                y = Y.T.reshape((-1,))
+                l = loss(outputs, y).mean()
+
+            l.backward()
+            grad_clipping(params, clipping_theta, ctx)
+            d2l.sgd(params, lr, 1)
+            l_sum += l.asscalar() * y.size
+            n += y.size
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f , time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start
+            ))
+            for prefix in prefixes:
+                print( ' -', predict_rnn(prefix, pred_len, rnn, params, init_rnn_state,
+                                        num_hiddens, vocab_size, ctx, idx_to_char, char_to_idx))
+
+num_epochs, num_steps, batch_size, lr, clipping_theta = 250, 35, 32, 1e2, 1e-2
+pred_period, pred_len, prefixes = 50, 50, ['分开', '不分开']
+
+# train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
+#                       vocab_size, ctx, corpus_indices, idx_to_char,
+#                       char_to_idx, True, num_epochs, num_steps, lr,
+#                       clipping_theta, batch_size, pred_period, pred_len,
+#                       prefixes)
+
+train_and_predict_rnn(rnn, get_params, init_rnn_state, num_hiddens,
+                      vocab_size, ctx, corpus_indices, idx_to_char,
+                      char_to_idx, False, num_epochs, num_steps, lr,
+                      clipping_theta, batch_size, pred_period, pred_len,
+                      prefixes)
