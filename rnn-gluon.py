c3ccd83b14205612c3102079fe6c518b7f124621
Yuzhi Sun <17745615596@163.com>
2020-10-05 11:12:37 +0800

    灵耀X

--- rnn-gluon.py
+++ rnn-gluon.py
@@ -35,3 +35,68 @@ class RNNModel(nn.Block):
     def begin_state(self, *args, **kwargs):
         return self.rnn.begin_state(*args, **kwargs)
 
+def predict_rnn_gluon(prefix, num_chars, model, vocab_size, ctx, idx_to_char,
+                      char_to_idx):
+    state = model.begin_state(batch_size=1, ctx=ctx)
+    output = [char_to_idx[prefix[0]]]
+    for t in range(num_chars + len(prefix) -1):
+        X = nd.array([output[-1]], ctx=ctx).reshape((1, 1))
+        (Y, state) = model(X, state)
+        if t < len(prefix) - 1:
+            output.append(char_to_idx[prefix[t+1]])
+        else:
+            output.append(int(Y.argmax(axis=1).asscalar()))
+    # return print(''.join([idx_to_char[i] for i in output]))
+    return ''.join([idx_to_char[i] for i in output])
+
+ctx = d2l.try_gpu()
+model = RNNModel(rnn_layer, vocab_size)
+model.initialize(force_reinit=True, ctx=ctx)
+predict_rnn_gluon('分开', 10, model, vocab_size, ctx, idx_to_char, char_to_idx)
+
+def train_and_predict_rnn_gluon(model, num_hiddens, vocab_size, ctx,
+                                corpus_indices, idx_to_char, char_to_idx,
+                                num_epochs, num_steps, lr, clipping_theta,
+                                batch_size, pred_period, pred_len, prefixes):
+    loss = gloss.SoftmaxCrossEntropyLoss()
+    model.initialize(ctx = ctx, force_reinit=True, init=init.Normal(0.01))
+    trainer = gluon.Trainer(model.collect_params(), 'sgd',
+                            {'learning_rate': lr, 'momentum': 0, 'wd': 0})
+
+    for epoch in range(num_epochs):
+        l_sum, n, start = 0.0, 0, time.time()
+        data_iter = d2l.data_iter_consecutive(
+            corpus_indices, batch_size, num_steps, ctx
+        )
+        state = model.begin_state(batch_size=batch_size, ctx=ctx)
+        for X, Y in data_iter:
+            for s in state:
+                s.detach()  # 从计算图分离隐藏状态
+            with autograd.record():
+                (output, state) = model(X, state)
+                y = Y.T.reshape((-1,))
+                l = loss(output, y).mean()
+            l.backward()
+
+            params = [p.data() for p in model.collect_params().values()]
+            d2l.grad_clipping(params, clipping_theta, ctx)
+            trainer.step(1)
+            l_sum += l.asscalar() * y.size
+            n += y.size
+
+        if (epoch + 1) % pred_period == 0:
+            print('epoch %d, perplexity %f, time %.2f sec' % (
+                epoch + 1, math.exp(l_sum / n), time.time() - start
+            ))
+            for prefix in prefixes:
+                print(' -', predict_rnn_gluon(
+                    prefix, pred_len, model, vocab_size, ctx, idx_to_char,
+                    char_to_idx
+                ))
+
+num_epochs, batch_size, lr, clipping_theta = 250, 32, 1e2, 1e-2
+pred_period, pred_len, prefixes = 50, 50, ['分开', '不分开']
+train_and_predict_rnn_gluon(model, num_hiddens, vocab_size, ctx,
+                            corpus_indices, idx_to_char, char_to_idx,
+                            num_epochs, num_steps, lr, clipping_theta,
+                            batch_size, pred_period, pred_len, prefixes)
