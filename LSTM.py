0d520ac8f5e05686dbbcca8a8fc92c196685c1c8
YuzhiSun <17745615596@163.com>
2020-10-08 00:12:46 +0800

    RTX change dim and axis

--- LSTM.py
+++ LSTM.py
@@ -1,9 +1,14 @@
 import d2lzh as d2l
 from mxnet import nd
 from mxnet.gluon import rnn
+import numpy
+
+
 
 (corpus_indices, char_to_idx, idx_to_char,
  vocab_size) = d2l.load_data_jay_lyrics()
+
+
 
 num_inputs, num_hiddens, num_outputs = vocab_size, 256, vocab_size
 ctx = d2l.try_gpu()
@@ -58,11 +63,11 @@ def lstm(inputs, state, params):
 num_epochs, num_steps, batch_size, lr, clipping_theta = 160, 35, 32, 1e2, 1e-2
 pred_period, pred_len, prefixes = 40, 50, ['分开', '不分开']
 
-d2l.train_and_predict_rnn(lstm, get_params, init_lstm_state, num_hiddens,
-                          vocab_size, ctx, corpus_indices, idx_to_char,
-                          char_to_idx, False, num_epochs, num_steps, lr,
-                          clipping_theta, batch_size, pred_period, pred_len,
-                          prefixes)
+# d2l.train_and_predict_rnn(lstm, get_params, init_lstm_state, num_hiddens,
+#                           vocab_size, ctx, corpus_indices, idx_to_char,
+#                           char_to_idx, False, num_epochs, num_steps, lr,
+#                           clipping_theta, batch_size, pred_period, pred_len,
+#                           prefixes)
 
 #简洁实现
 lstm_layer = rnn.LSTM(num_hiddens)
--- utils.py
+++ utils.py
@@ -2,17 +2,25 @@ import pandas as pd
 import numpy as np
 from mxnet import nd
 from mxnet.gluon import nn
-nn.Dense()
-# 交换坐标轴 下面记录的是 如何将（batchsize，vocab_size，num_steps）变成 （num_steps,batchsize, vocab_size）
-# numpy版本
-a = np.arange(1,25).reshape(2,3,4)
-print(a,'\n')
-aT = np.transpose(a,[2,0,1])
-# NDarray版本
-b = nd.arange(1, 25).reshape(2,3,4)
-bT = nd.transpose(b, [2, 0, 1])
-print(aT)
-for metricb in bT:
-    print(metricb)
-for metric in aT:
-    print(metric)
+def change_axis():
+    # 交换坐标轴 下面记录的是 如何将（batchsize，vocab_size，num_steps）变成 （num_steps,batchsize, vocab_size）
+    # numpy版本
+    a = np.arange(1,25).reshape(2,3,4)
+    print(a,'\n')
+    aT = np.transpose(a,[2,0,1])
+    # NDarray版本
+    b = nd.arange(1, 25).reshape(2,3,4)
+    bT = nd.transpose(b, [2, 0, 1])
+    print(aT)
+    for metricb in bT:
+        print(metricb)
+    for metric in aT:
+        print(metric)
+
+def change_dim():
+    # 改变维度  将（bz,roads(69),times） 变成 （bz,3,23,times）
+    data = nd.arange(0,36).reshape((2,6,3))
+    print('origin data:',data,'\n')
+    data = data.reshape((2,3,2,3))
+    print('change data：',data)
+change_dim()
